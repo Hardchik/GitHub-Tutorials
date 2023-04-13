d1184ef26e74015b39891bbc2243d61c6b142855
Vishisht Chhabra <chhabrahardik345@gmail.com>
2023-04-14 01:02:15 +0530

    function definition

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+console.log("Hello World!");
+
+function Global() {
+  var a = 5;
+  var arr = [];
+  for (let i = 0; i < a; i++) {
+    arr.push(i);
+  }
+  arr.forEach((array) => console.log(array));
+}
+
+Global();
